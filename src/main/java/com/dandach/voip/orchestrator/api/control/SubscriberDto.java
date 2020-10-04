a8f5e5b7e533722191cad8228fe7fffe3092db54
ali <aliakbar.dandach@isae.edu.lb>
2020-10-04 09:47:04 +0300

    add dto

--- src/main/java/com/dandach/voip/orchestrator/api/control/SubscriberDto.java
+++ src/main/java/com/dandach/voip/orchestrator/api/control/SubscriberDto.java
@@ -1,4 +1,22 @@
 package com.dandach.voip.orchestrator.api.control;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubscriberDto {
+    @Getter
+    @Setter
+    @Accessors(chain = true)
+    public static class Add{
+        private String firstName;
+        private String lastName;
+        private Character gender;
+        private List<String> emails=new ArrayList<>();
+        private String username;
+        private String password;
+    }
 }
--- src/main/java/com/dandach/voip/orchestrator/api/control/SubscriberService.java
+++ src/main/java/com/dandach/voip/orchestrator/api/control/SubscriberService.java
@@ -1,14 +1,23 @@
 package com.dandach.voip.orchestrator.api.control;
 
+import com.dandach.voip.orchestrator.api.control.credential.AccountRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class SubscriberService {
+    private final AccountRepository accountRepository;
     private final SubscriberRepository subscriberRepository;
 
-    public void addSubscriber(){
+    public void addSubscriber(SubscriberDto.Add dto){
 
+    }
+
+    public void view(long subscriberId){
+
+    }
+
+    public void edit(){
     }
 }
